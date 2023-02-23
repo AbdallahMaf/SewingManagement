84c3f6506b2ddf93affaf04a37ca4d857d8343e2
Mohamed Abdallah Fall <abdallahfall2020@gmail.com>
2023-02-23 20:12:13 +0000

    DataBaseFirst Create Model

--- AtelierCoutureGEST/Program.cs
+++ AtelierCoutureGEST/Program.cs
@@ -16,7 +16,7 @@ namespace AtelierCoutureGEST
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Mesures());
+            Application.Run(new Acceuil());
         }
     }
 }
